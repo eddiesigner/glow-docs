28c690f5bdc32acff2e68d6480ecad9c94313ff6
Eduardo Gomez <this.eduardo@gmail.com>
2024-01-20 12:52:59 +0100

    Update docs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Glow Ghost Theme",
+  title: "Glow Theme Documentation",
   description: "Theme official documentation",
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
